865a5bf762c46c9b1cbc9f9de905b2c623a306cc
Theunis van Niekerk <theunis.vanniekerk@outlook.com>
2017-03-28 18:13:50 +0200

    Added Nuspec

---
+++ tvn-cosine.imaging.leptonica/tvn-cosine.imaging.leptonica/Native.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tvn.Cosine.Imaging.Leptonica
+{
+    class Native
+    {
+    }
+}
--- tvn-cosine.imaging.leptonica/tvn-cosine.imaging.leptonica/Properties/AssemblyInfo.cs
+++ tvn-cosine.imaging.leptonica/tvn-cosine.imaging.leptonica/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("tvn-cosine.core")]
-[assembly: AssemblyDescription("Core functionality of tvn-cosine")]
+[assembly: AssemblyTitle("tvn-cosine.imaging.leptonica")]
+[assembly: AssemblyDescription("Leptonica implementation of Leptonica")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("tvn-cosine")]
-[assembly: AssemblyProduct("tvn-cosine.core")]
+[assembly: AssemblyProduct("tvn-cosine.imaging.leptonica")]
 [assembly: AssemblyCopyright("Copyright © tvn-cosine 2017")]
 [assembly: AssemblyTrademark("tvn-cosine 2017")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f8b67fe9-d126-44ac-9f9b-67e0efa5083a")]
+[assembly: Guid("05208a47-03a0-4497-ba35-57bf3706be72")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
