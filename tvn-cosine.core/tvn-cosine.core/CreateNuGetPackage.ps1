6e56b24727808202ae2b66bded5b57d977bd52c7
Theunis van Niekerk <theunis.vanniekerk@outlook.com>
2017-03-28 18:18:20 +0200

    NuGet

---
+++ tvn-cosine.core/tvn-cosine.core/CreateNuGetPackage.ps1
@@ -0,0 +1,77 @@
+﻿Write-Host "########"
+
+$projectName = "tvn-cosine.core"
+$GitExe = "C:\Program Files\Git\bin\git.exe"
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" 
+$localDirectory = "C:\csource\"
+$GitHttpsUrl = "https://github.com/tvn-cosine/imaging"
+ 
+Write-Host "########"
+Write-Host "Creating directory $localDirectory ..."
+
+if (Test-Path "$localDirectory\$projectName")
+{
+    Remove-Item "$localDirectory\$projectName" -Force -Recurse
+    Write-Host "Done removing directory..."
+} 
+
+if (-not (Test-Path "$localDirectory"))
+{
+    New-Item $localDirectory -ItemType Directory
+    Write-Host "Done creating directory..."
+}
+Write-Host "########"
+Write-Host " "
+Write-Host " "
+ 
+Write-Host "########"
+Write-Host "Changine to directory $localDirectory..." 
+Set-Location -Path $localDirectory
+Write-Host "Changed to directory..." 
+Write-Host "########"
+Write-Host " "
+Write-Host " "
+
+ 
+
+Write-Host "########"
+Write-Host "Cloning from git $localDirectory."
+$localDirectory = "C:\csource\$projectName"
+
+Try
+{
+    $erroractionpreference = "Stop"
+    & $GitExe clone "$GitHttpsUrl" 
+} Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    Write-Host "$ErrorMessage..." 
+}
+    $erroractionpreference = "Stop"
+
+Write-Host "Cloned into $localDirectory ..." 
+Write-Host "########"
+Write-Host " "
+Write-Host " "
+
+Write-Host "########"
+Write-Host "Downloading nuget into $localDirectory."
+$targetNugetExe = "$localDirectory\nuget.exe"
+if (-Not (Test-Path $targetNugetExe))
+{
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe 
+}
+Write-Host "Downloaded nuget..." 
+Write-Host "########"
+Write-Host " "
+Write-Host " "
+ 
+ 
+Write-Host "########"
+Write-Host "Packing nuget into $localDirectory." 
+& $targetNugetExe pack "$localDirectory\$projectName\$projectName.csproj" -build
+Write-Host "Downloaded nuget..." 
+Write-Host "########"
+Write-Host " "
+Write-Host " " 
+Write-Host "Done..."
