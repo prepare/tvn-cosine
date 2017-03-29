13af08cdb39936f3e6b01c5d64b2a6f56525cff1
tvn-cosine <tvanniekerk@gmail.com>
2017-03-29 11:53:03 +0200

    Add files via upload

---
+++ tvn-cosine.ocr/tvn-cosine.ocr.tesseract/PdfRenderer.cs
@@ -0,0 +1,9 @@
+﻿namespace tvn_cosine.ocr.tesseract
+{
+    public class PdfRenderer : ResultRenderer
+    {
+        public PdfRenderer(string fileName, string dataDir, bool textOnly)
+            : base(Native.DllImports.TessPDFRendererCreate(fileName, dataDir, textOnly ? 1 : 0))
+        { }
+    }
+}
---
+++ tvn-cosine.ocr/tvn-cosine.ocr.tesseract/UnlvRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace tvn_cosine.ocr.tesseract
+{
+    public class UnlvRenderer : ResultRenderer
+    {
+        public UnlvRenderer(string fileName)
+        {
+            IntPtr pointer = Native.DllImports.TessUnlvRendererCreate(fileName);
+            handleRef = new HandleRef(this, pointer);
+        }
+    }
+}
