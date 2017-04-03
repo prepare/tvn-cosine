81f7289fda1488da3d9c5a2c3758c31442f62f15
tvn-cosine <tvn-cosine>
2017-04-03 08:15:42 +0200

    zone order

--- tvn-cosine.wpf/tvn-cosine.wpf/Views/UserControls/Zone.xaml.cs
+++ tvn-cosine.wpf/tvn-cosine.wpf/Views/UserControls/Zone.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Controls; 
 using System.Windows.Media;
-using Tvn.Cosine.Data;
-using Tvn.Cosine.Geometry;
+using Tvn.Cosine.Data; 
 using Tvn.Cosine.Imaging;
 
 namespace Tvn.Cosine.Wpf.Views.UserControls
